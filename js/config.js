96d8ea981df645f700066f8707e7d3815d12e664
RamsaiBoddu <144872511+RamsaiBoddu@users.noreply.github.com>
2025-04-15 19:11:01 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_fdr4806Rm', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5juk3k3tm0jr5uvuhk4j0n9jnv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
