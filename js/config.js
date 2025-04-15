054de255c7d7eef83bebc5fe70a1f0d8885ca9bc
RamsaiBoddu <144872511+RamsaiBoddu@users.noreply.github.com>
2025-04-15 19:15:42 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_fdr4806Rm', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5juk3k3tm0jr5uvuhk4j0n9jnv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '5j9n6nt5fkslhesrt3q68p0isc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
